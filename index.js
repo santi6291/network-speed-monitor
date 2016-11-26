6a9fa39609c7644f7b047821ddfa401038af86a5
Santiago J Sosa <ssosa@santiagojsosa.com>
2016-11-25 23:28:51 -0500

    more logs

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const SpeedTask = require('./libs/speedTest');
 const EmailTask = require('./libs/email');
 
 const speedCron = cron.scheduleJob(process.env.CRON_SPEED_TEST_SCHEDULE, ()=>{
+	console.log('cron stask - SpeedTask');
 	new SpeedTask();
 })
 const emailCron = cron.scheduleJob(process.env.CRON_EMAIL_RESULTS_SCHEDULE, ()=>{
+	console.log('cron stask - EmailTask');
 	new EmailTask();
 })
