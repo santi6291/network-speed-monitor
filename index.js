da487ebeb4415c4dbe78efa6a47419ea31ec43a4
Santiago J Sosa <ssosa@santiagojsosa.com>
2016-12-29 15:45:16 -0500

    use node-cron package

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
-const cron = require('node-schedule');
+
+// const cron = require('node-schedule');
+const CronJob = require('cron').CronJob;
 
 const SpeedTask = require('./libs/speedTest');
 const EmailTask = require('./libs/email');
 const log = require('./libs/log');
+ 
+const timezone = 'America/New_York';
 
-log('Setting: speedCron');
+/*log('Setting: speedCron');
 const speedCron = cron.scheduleJob(process.env.CRON_SPEED_TEST_SCHEDULE, ()=>{
 	log('cron stask - SpeedTask');
 	new SpeedTask();
@@ -16,3 +20,24 @@ const emailCron = cron.scheduleJob(process.env.CRON_EMAIL_RESULTS_SCHEDULE, ()=>
 	log('cron stask - EmailTask');
 	new EmailTask();
 })
+*/
+
+log('Setting: speedCron');
+const speedCron = new CronJob(process.env.CRON_SPEED_TEST_SCHEDULE, ()=> {
+	log('cron stask - SpeedTask');
+	new SpeedTask();
+  },
+  null, /* Call bacl after cron is finished */
+  true, /* Start the job right now */
+  timezone /* Time zone of this job. */
+);
+
+log('Setting: emailCron');
+const emailCron = new CronJob(process.env.CRON_EMAIL_RESULTS_SCHEDULE, ()=> {
+	log('cron stask - EmailTask');
+	new EmailTask();
+  },
+  null, /* Call bacl after cron is finished */
+  true, /* Start the job right now */
+  timezone /* Time zone of this job. */
+);
