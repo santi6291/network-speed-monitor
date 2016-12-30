f13b20414670ce173f42988eed3493ef3e025aec
Santiago J Sosa <ssosa@santiagojsosa.com>
2016-12-30 13:40:37 -0500

    write msg to file

--- libs/log.js
+++ libs/log.js
@@ -1,7 +1,11 @@
 const moment = require('moment');
+const fs = require('fs');
 
 module.exports = (msg)=>{
 	const format = 'YY-MM-DD - HH:mm:ss.SSS';
 	const timestamp = moment().format(format);
-	return console.log(`[${timestamp}] `, msg);
+	const msgString = `[${timestamp}] ${msg}\n`;
+	
+	console.log(msgString);
+	return fs.appendFileSync('./logs/logs.log', msgString);
 }
