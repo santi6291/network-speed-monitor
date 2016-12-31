8319ed4e146eb3a3e702bd002ad6028d22f40b96
Santiago J Sosa <ssosa@santiagojsosa.com>
2016-12-31 13:50:12 -0500

    index log

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const argv = require('yargs').argv;
 
+const log = require(process.env.PROJECT_ROOT+'/libs/log');
+
 const SpeedTask = require(process.env.PROJECT_ROOT+'/libs/speedTest');
 const EmailTask = require(process.env.PROJECT_ROOT+'/libs/email');
  
-log('Cron executed')
+log('Cron executing');
 if (argv.email) {
 	new EmailTask();
 } else {
