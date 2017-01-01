fa08fbeecd322261cd2eed05b1746a47c2cab073
Santiago J Sosa <ssosa@santiagojsosa.com>
2017-01-01 14:59:57 -0500

    write logs via crontab

--- libs/log.js
+++ libs/log.js
@@ -6,8 +6,8 @@ const fs = require('fs');
 module.exports = (msg)=>{
 	const format = 'YY-MM-DD - HH:mm:ss.SSS';
 	const timestamp = moment().format(format);
-	const msgString = `[${timestamp}] ${msg}\n`;
+	const msgString = `[${timestamp}] ${msg}`;
 	
 	console.log(msgString);
-	return fs.appendFileSync(`${process.env.PROJECT_ROOT}${process.env.LOGS_PATH}/logs.log`, msgString);
+	// return fs.appendFileSync(`${process.env.PROJECT_ROOT}${process.env.LOGS_PATH}/logs.log`, msgString);
 }
